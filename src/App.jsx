d51b3188b80dc33c4b81b8ce0bd6c4d16000deba
Federico-Pedro <federicopedroroveda@gmail.com>
2025-10-19 09:13:36 -0300

    Crea el tablero, los jugadores, el cambio de turno, la funcion handle click y el boton para limpiar el tablero. Tambien la estética futurista

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,78 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [players, setPlayers] = useState(
+    [
+      { name: 'Player1', token: 'X' },
+      { name: 'Player2', token: 'O' }
+    ]
+  )
+
+
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0)
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+
+  const nextTurn = () => {
+    setCurrentPlayerIndex(prev => prev === 0 ? 1 : 0
+    )
+  }
+
+  const currentPlayer = players[currentPlayerIndex]
+
+
+  const handleClick = (index) => {
+    console.log('Click en casilla', index)
+    const newBoard = [...board]
+    newBoard[index] = currentPlayer.token
+    setBoard(newBoard)
+    nextTurn()
+
+  }
+
+
+  const restart = () => {
+    const cleanBoard = Array(9).fill(null)
+    setBoard(cleanBoard)
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className='main-container'>
+
+        <div>
+          <h4>
+            Turno de: {currentPlayer.name}
+          </h4>
+
+        </div>
+
+        <div className='container'>
+
+          <div className='board-container'>
+
+            {board.map((value, index) => (
+              <button
+                className='box'
+                disabled={value !== null}
+                key={index}
+                onClick={() => handleClick(index)}
+              >
+                {value}
+              </button>
+            ))}
+          </div>
+        </div>
+        <button className='restart'
+          onClick={() => restart()}
+        >
+          Reiniciar
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
