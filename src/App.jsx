90cb9427038c2c7fc9bd3335afe431df6fa2b18e
Federico-Pedro <federicopedroroveda@gmail.com>
2025-10-20 12:24:32 -0300

    Agrega funcionalidad en el modal para ingresar nombres de jugadores

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
+
 function App() {
 
   const [players, setPlayers] = useState(
@@ -14,8 +15,14 @@ function App() {
   const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0)
   const [board, setBoard] = useState(Array(9).fill(null))
   const [winner, setWinner] = useState(null)
+  const [showmodal, setShowModal] = useState(true)
   const currentPlayer = players[currentPlayerIndex]
 
+
+
+  const startGame = () => {
+    setShowModal(!showmodal)
+  }
 
   const nextTurn = () => {
     setCurrentPlayerIndex(prev => prev === 0 ? 1 : 0
@@ -31,7 +38,7 @@ function App() {
     setBoard(newBoard)
     const isWinner = checkWinner(newBoard)
 
-    if(isWinner) {
+    if (isWinner) {
       setWinner(isWinner)
       console.log(winner + " GANA LA PARTIDA")
       return
@@ -47,7 +54,7 @@ function App() {
     setBoard(cleanBoard)
     setWinner(null)
     setCurrentPlayerIndex(0)
-    
+
   }
 
 
@@ -64,49 +71,82 @@ function App() {
       (board[2] !== null && board[2] === board[4] && board[4] === board[6])
     ]
 
-   const hasWinner = winPossibilities.some(w => w)
+    const hasWinner = winPossibilities.some(w => w)
 
-   if(hasWinner){
-    return currentPlayer.name
-   }
+    if (hasWinner) {
+      return currentPlayer.name
+    }
 
-   return null
+    return null
   }
 
   return (
     <>
-      <div className='main-container'>
+      {showmodal ? (
 
-        <div>
-          <h4>
-            {winner ? winner + " GANA LA PARTIDA" : "Turno de: " + players[currentPlayerIndex].name + " (" + players[currentPlayerIndex].token + ")"}
+        <div className='modal'>
+          <h4 className="title">
+            Bienvenidos al TA-TE-TI
           </h4>
-
-        </div>
-
-        <div className='container'>
-
-          <div className='board-container'>
-
-            {board.map((value, index) => (
-              <button
-                className='box'
-                disabled={(value !== null) || winner}
-                key={index}
-                onClick={() => handleClick(index)}
-              >
-                {value}
-              </button>
-            ))}
+          <div className="input-container">
+            <label className="input-name" htmlFor="player1"> Jugador 1:
+              <input type="text" name="player1" placeholder="Player 1"
+                onChange={(e) => {
+                  const newPlayers = [...players] 
+                  newPlayers[0].name = e.target.value  
+                  setPlayers(newPlayers) 
+                }} />
+            </label>
+            <label className="input-name" htmlFor="player2"> Jugador 2:
+              <input type="text" name="player2" placeholder="Player 2"
+                onChange={(e) => {
+                  const newPlayers = [...players] 
+                  newPlayers[1].name = e.target.value  
+                  setPlayers(newPlayers) 
+                }} />
+            </label>
+          </div>
+          <div className='modalButtonContainer'>
+            <button className='modalButton' onClick={() => startGame()}>Comenzar</button>
           </div>
         </div>
-        <button className='restart'
-          onClick={() => restart()}
-        >
-          Reiniciar
-        </button>
+      ) : (
 
-      </div>
+
+
+
+        <div className='main-container'>
+          <div>
+            <h4>
+              {winner ? winner + " GANA LA PARTIDA" : "Turno de: " + players[currentPlayerIndex].name + " (" + players[currentPlayerIndex].token + ")"}
+            </h4>
+
+          </div>
+
+          <div className='container'>
+
+            <div className='board-container'>
+
+              {board.map((value, index) => (
+                <button
+                  className='box'
+                  disabled={(value !== null) || winner}
+                  key={index}
+                  onClick={() => handleClick(index)}
+                >
+                  {value}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className='restart'
+            onClick={() => restart()}
+          >
+            Reiniciar
+          </button>
+
+        </div>
+      )}
 
     </>
   )
